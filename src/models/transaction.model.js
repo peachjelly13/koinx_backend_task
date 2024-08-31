c1c3963107dcf88ef661eb22de464a2e87779b01
peachjelly13 <ananyasharma112233441234@gmail.com>
2024-08-31 23:20:26 +0530

    create transaction schema

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -14,6 +14,6 @@ const transactionSchema = new Schema({
 
 },{timeStamps:true})    
 
-export const User = mongoose.model("User",transactionSchema)
+export const transactions = mongoose.model("transactions",transactionSchema)
 
 
