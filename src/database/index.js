21f059d0eacda4ee3fe00d3121ba2e9420a53995
peachjelly13 <ananyasharma112233441234@gmail.com>
2024-09-01 16:34:53 +0530

    create database connection with mongodb

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async() =>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`\n Mongo Db connected DB host:${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection Error")
+        process.exit(1)
+        
+    }
+}
+
+export default connectDB;
